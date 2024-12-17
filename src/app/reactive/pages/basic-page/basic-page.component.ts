03695b0eeaf1ad1fbe733d95484e62f131794f17
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-17 09:53:39 +0100

    Formularios Reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  })
+
+  // De esta forma se puede utilizar el form builder, pero da problema, asi que utilizare formcontrol.
+  // public myForm: FormGroup = this.fb.group({
+  //   name: [''],
+  //   price: [0],
+  //   inStorage: [0]
+  // })
+
+  // constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
