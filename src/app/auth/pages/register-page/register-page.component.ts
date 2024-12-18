3a34ffad4b71c90f39dbae42bbb23a120b86929c
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-18 13:26:38 +0100

    Validators Functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators.helpers';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,8 +10,8 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, ]],
-    email: ['', [Validators.required, ]],
-    username: ['', [Validators.required]],
+    email: ['', [Validators.required ]],
+    username: ['', [Validators.required, cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   })
