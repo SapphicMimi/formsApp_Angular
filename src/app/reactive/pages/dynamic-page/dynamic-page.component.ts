ae5d055bc85263f5ccf2b2839eb3cd1fcdaf5ab7
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-18 08:42:18 +0100

    Validaciones Dinámicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -10,6 +10,14 @@ export class DynamicPageComponent {
   // public myForm2 = new FormGroup({
   //   favoriteGames: new FormArray([])
   // });
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  //   favoriteGames: new FormArray([
+  //     new FormControl('Metal Gear', [Validators.required]),
+  //     new FormControl('Death Stranding', [Validators.required]),
+  //   ])
+  // })
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -25,9 +33,36 @@ export class DynamicPageComponent {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return null;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
   onSubmit(): void {
     if(this.myForm.invalid) {
-      this.myForm.markAsTouched();
+      this.myForm.markAllAsTouched();
       return;
     }
 
