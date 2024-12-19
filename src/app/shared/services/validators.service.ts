fb945fdcb73033dfc3305efa3f7fc42bada1d6f8
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-19 11:59:06 +0100

    Comprobar que dos campos sean iguales

--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
 export class  ValidatorsService {
@@ -20,5 +20,22 @@ export class  ValidatorsService {
   public isValidField(form: FormGroup, field: string) {
     return form.controls[field].errors
       && form.controls[field].touched;
+  }
+
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+
+    return (FormGroup: AbstractControl): ValidationErrors | null => {
+      const fieldValue1 = FormGroup.get(field1)?.value;
+      const fieldValue2 = FormGroup.get(field2)?.value;
+
+      if(fieldValue1 !== fieldValue2 ) {
+        FormGroup.get(field2)?.setErrors({notEqual: true})
+        return {notEqual: true}
+      }
+
+      FormGroup.get(field2)?.setErrors(null);
+
+      return null;
+    }
   }
 }
